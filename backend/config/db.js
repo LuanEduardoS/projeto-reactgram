3b83002b1eea03e5b9385c6006d3f3399d627516
LuanEduardoS <luaneduardolima186@gmail.com>
2023-07-29 21:42:39 -0300

    novo BD

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,7 @@ const dbPassword = process.env.DB_PASS
 const conn = async () => {
     try {
         const dbConn = await mongoose.connect(
-            `mongodb+srv://${dbUser}:${dbPassword}@cluster0.8rb5vx4.mongodb.net/?retryWrites=true&w=majority`
+            `mongodb+srv://${dbUser}:${dbPassword}@cluster0.zezijbp.mongodb.net/?retryWrites=true&w=majority`
         )
         console.log("Conectou ao banco!")
         
